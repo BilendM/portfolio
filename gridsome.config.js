e9acbbf2e027637307a5e33f19e86874962639e4
bilendm <bilendmohammed@gmail.com>
2021-08-02 19:03:39 +0300

    Added sitemap and robots.txt

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,9 +6,9 @@
 
 module.exports = {
   siteName: 'Bilend Mohammed',
+  siteUrl: 'https://bilendm.com',
   titleTemplate: "Bilend Mohammed",
   siteDescription: 'Bilend Mohammed is a full-stack web developer specializing in front-end technologies based in Sulaymaniyah, Iraq',
-  siteUrl: process.env.DEPLOY_URL || 'https://bilendm.com',
   metadata: {
     twitter: {
       site: '@bilendm',
@@ -23,6 +23,22 @@ module.exports = {
         typeName: 'works',
         remark: {
           //Config options can be added here
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        config: {
+          '#about': {
+            lastmod: '2021-08-02',
+          },
+          '#works': {
+            lastmod: '2021-08-02',
+          },
+          '#contact': {
+            lastmod: '2021-08-02',
+          }
         }
       }
     },
